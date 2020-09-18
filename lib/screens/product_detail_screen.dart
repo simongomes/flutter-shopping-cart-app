591e0f4f3ffd423e3c4baa4bfc4419e6c6c650cb
SIMON GOMES <busy.s.simon@gmail.com>
2020-09-18 22:26:44 +0600

    Data added in the Product Details screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
@@ -18,6 +19,21 @@ class ProductDetailScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(loadedProduct.title),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover,),
+            ),
+            SizedBox(height: 10,),
+            Text("\$${loadedProduct.price}", style: TextStyle(color: Colors.grey, fontSize: 20),),
+            SizedBox(height: 10,),
+            Container(width: double.infinity, padding: EdgeInsets.symmetric(horizontal: 20), child: Text(loadedProduct.description, textAlign: TextAlign.center, softWrap: true,))
+          ],
+        ),
+      ),
     );
   }
 }
