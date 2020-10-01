94efd3653626233e15b3bb0804d1b59ec803012c
SIMON GOMES <busy.s.simon@gmail.com>
2020-10-01 22:14:07 +0600

    sendingproduct favorite update request on the server

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier{
   final String id;
@@ -17,8 +20,30 @@ class Product with ChangeNotifier{
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = 'https://flutter-shop-app-b3619.firebaseio.com/products/$id.json';
+
+    try {
+      final response = await http.patch(
+          url,
+          body: json.encode({
+            'isFavorite': isFavorite,
+          })
+      );
+      if(response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
